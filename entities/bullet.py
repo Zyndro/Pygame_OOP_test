4599e0620e81e76d7abe5b4bf516d128d69fdcbf
Zyndro <lis.mateusz@ymail.com>
2019-06-07 20:34:22 +0200

    collisions update, game restarts properly

--- entities/bullet.py
+++ entities/bullet.py
@@ -6,12 +6,16 @@ class bullets():
         self.y = (480 - 45)
         self.vel=5
         self.pocisk = pygame.image.load('assets/bullet.png')
+        self.collided=False
 
     def update(self,x ,win):
         self.x=x
         self.y -= self.vel
         self.win = win
-        self.win.blit(self.pocisk, (self.x+13, self.y))
+        if self.collided==False:
+            self.win.blit(self.pocisk, (self.x+13, self.y))
+
+
 
     def collisionx(self):
         return (self.x)
